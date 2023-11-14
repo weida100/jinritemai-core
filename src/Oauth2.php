c16c4ffcb4478f701b0d08d3b51f39b7a0b696ae
Weida <sgenmi@gmail.com>
2023-11-14 12:03:50 +0800

    fix:获取 token时，场景参数

--- src/Oauth2.php
+++ src/Oauth2.php
@@ -19,6 +19,7 @@ use Weida\JinritemaiCore\Contract\AccessTokenInterface;
 
 class Oauth2 extends AbstractApplication
 {
+    protected array $otherAuthParams=[];
 
     private AccessTokenInterface $accessToken;
     public function __construct(array|ConfigInterface $config,AccessTokenInterface $accessToken)
@@ -46,17 +47,27 @@ class Oauth2 extends AbstractApplication
         $params=[
             'method'=>'token.create',
             'app_key'=> $this->getConfig()->get('client_id'),
-            'param_json'=>[
-                'code'=>$code,
-                'grant_type'=>'authorization_code',
-            ],
-            'timestamp'=>time(),
+            'timestamp'=>date("Y-m-d H:i:s"),
             'sign_method'=>'hmac-sha256',
             'v'=>'2'
         ];
+        if($this->getConfig()->get('app_type')=='self') {
+            $params['param_json']=[
+                'shop_id'=>$code,
+                'grant_type'=>'authorization_self',
+                'code'=>''
+            ];
+
+        }else {
+            $params['param_json'] = [
+                'grant_type' => 'authorization_code',
+                'code' => $code
+            ];
+        }
+        $params['param_json'] = array_merge($params['param_json'],$this->otherAuthParams);
         $params['sign'] = Encryptor::Sign($params,$this->getConfig()->get('client_secret'));
         $params['param_json'] = json_encode($params['param_json']);
-        return 'https://openapi-fxg.jinritemai.com?'.http_build_query($params);
+        return 'https://openapi-fxg.jinritemai.com/token/create?'.http_build_query($params);
     }
 
 
@@ -110,13 +121,33 @@ class Oauth2 extends AbstractApplication
             throw new RuntimeException('Failed to get access_token: ' . json_encode($arr, JSON_UNESCAPED_UNICODE));
         }
         $callback = $this->getConfig()->get('access_token_callback');
-        if($callback && is_callable($callback)){
+        if($callback && (is_callable($callback) || is_array($callback))){
             try {
-                call_user_func($callback,$arr['data']);
+                if(is_array($callback)){
+                    $obj = $callback[0];
+                    $action = $callback[1]??'';
+                    if($obj && $action){
+                        ( new $obj())->$action($arr['data']);
+                    }
+                }else{
+                    call_user_func($callback,$arr['data']);
+                }
             }catch (Throwable $e){
             }
         }
         $this->accessToken->saveCache($arr['data']['shop_id'],$arr['data']['access_token'],intval($arr['data']['expires_in']));
         return $arr;
     }
+
+    /**
+     * @param array $params
+     * @return $this
+     * @author Weida
+     */
+    public function setOtherAuthParams(array $params):static{
+        $this->otherAuthParams = $params;
+        return $this;
+    }
+
+
 }
