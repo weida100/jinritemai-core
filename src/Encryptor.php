575f5bce3136314a699c47dabda58177f9059e3c
Weida <sgenmi@gmail.com>
2023-11-14 12:03:25 +0800

    fix:数组参数，加密

--- src/Encryptor.php
+++ src/Encryptor.php
@@ -20,11 +20,33 @@ class Encryptor
     public static function sign(array $params,string $secret):string{
         unset($params["sign"]);
         $paramJsonArr = $params['param_json'];
-        ksort($paramJsonArr);
-        $paramJson= json_encode($paramJsonArr);
+
+        if(is_string($paramJsonArr)){
+            $paramJsonArr = json_decode($paramJsonArr,true);
+        }
+        self::recKSort($paramJsonArr);
+        $paramJson= json_encode($paramJsonArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
         $paramPattern = 'app_key'.$params['app_key'].'method'.$params['method'].'param_json'.$paramJson.'timestamp'.$params['timestamp'].'v'.$params['v'];
         $signPattern = $secret.$paramPattern.$secret;
         return hash_hmac("sha256", $signPattern, $secret);
+    }
+
+    // 关联数组排序，递归
+    private static function recKSort(&$arr): void
+    {
+        $kString = true;
+        foreach ($arr as $k => &$v) {
+            if (!is_string($k)) {
+                $kString = false;
+            }
+            if (is_array($v)) {
+                self::recKSort($v);
+            }
+        }
+        unset($v);
+        if ($kString) {
+            ksort($arr);
+        }
     }
 
 }
